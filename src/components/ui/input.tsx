e651805d435623068e541ea069b0de0558d65f4d
Leonardo Geja <bemmenos@gmail.com>
2024-11-16 09:07:45 -0300

    feat: add vercel.json

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  wrapperClassName?: string
+  className?: string
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
